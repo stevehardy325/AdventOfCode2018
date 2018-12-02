1778282a1bd7b7dfef5e5e969cf04bfb1788c331
Steve Hardy <stevehardy325@gmail.com>
2018-12-02 13:29:53 -0500

    Day 1, Part 2 solution

--- day_01/day01.py
+++ day_01/day01.py
@@ -2,14 +2,52 @@
 
 # https://adventofcode.com/2018/day/1
 
-# add together all frequency changes in the input file
+# Part 1: add together all frequency changes in the input file
+# Part 2: find first repeated frequency reached after a change
 
 filename = "input"
 file = open(filename, "r")
+
+#sum for part 1
 sum = 0
+
+#track repeats for part 2
+freqs = dict()
+repeated = None
+
+#int array for faster parsing if we need to loop later
+intLines = []
 
 for line in file:
     num = int(line)
+    intLines += [num]
+    #calculate part 1
     sum += num
-    
-print(sum)
+    #see if the sum is repeated for part 2
+    if repeated == None:
+        if sum not in freqs:
+            freqs[sum] = 1
+        else:
+            freqs[sum] += 1
+            repeated = sum
+
+#we already know the end sum
+print("Frequency sum: " + str(sum))
+
+#if we don't have the repeated yet, keep cycling until we do
+index = 0
+while repeated == None:
+    #use the int array, because it will be faster
+    num = intLines[index]
+
+    sum += num
+    if repeated == None:
+        if sum not in freqs:
+            freqs[sum] = 1
+        else:
+            freqs[sum] += 1
+            repeated = sum
+    index += 1
+    if index == len(intLines):  index = 0
+
+print("First repeated frequency: " + str(repeated))
