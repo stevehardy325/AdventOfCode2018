c244363d3f48cdded6723b8d906a8c96443e5a61
Steve Hardy <stevehardy325@gmail.com>
2018-12-02 13:56:22 -0500

    Day 1 - Abstracted some logic into a class

--- day_01/day01.py
+++ day_01/day01.py
@@ -5,15 +5,31 @@
 # Part 1: add together all frequency changes in the input file
 # Part 2: find first repeated frequency reached after a change
 
+class Results:
+    def __init__(self):
+        #sum for part 1
+        self.sum = 0
+        #track repeats for part 2
+        self.freqs = dict()
+        self.repeated = None
+
+    def changeFreq(self, n):
+        #calculate part 1
+        self.sum += num
+        #see if the sum is repeated for part 2
+        if self.sum not in self.freqs:
+            self.freqs[self.sum] = 1
+        else:
+            self.freqs[self.sum] += 1
+            if self.repeated == None:
+                self.repeated = self.sum
+
+
 filename = "input"
 file = open(filename, "r")
 
-#sum for part 1
-sum = 0
-
-#track repeats for part 2
-freqs = dict()
-repeated = None
+#object to hold results
+res = Results()
 
 #int array for faster parsing if we need to loop later
 intLines = []
@@ -21,33 +37,21 @@ intLines = []
 for line in file:
     num = int(line)
     intLines += [num]
-    #calculate part 1
-    sum += num
-    #see if the sum is repeated for part 2
-    if repeated == None:
-        if sum not in freqs:
-            freqs[sum] = 1
-        else:
-            freqs[sum] += 1
-            repeated = sum
+    res.changeFreq(num)
+
+file.close()
 
 #we already know the end sum
-print("Frequency sum: " + str(sum))
+print("Frequency sum: " + str(res.sum))
 
 #if we don't have the repeated yet, keep cycling until we do
 index = 0
-while repeated == None:
+while res.repeated == None:
     #use the int array, because it will be faster
     num = intLines[index]
+    res.changeFreq(num)
 
-    sum += num
-    if repeated == None:
-        if sum not in freqs:
-            freqs[sum] = 1
-        else:
-            freqs[sum] += 1
-            repeated = sum
     index += 1
     if index == len(intLines):  index = 0
 
-print("First repeated frequency: " + str(repeated))
+print("First repeated frequency: " + str(res.repeated))
